abba10813686cddba196a88fb1ee3822beb67d2d
dev-msj <msj944@naver.com>
2023-01-11 13:22:28 +0900

    Project 생성

---
+++ src/main/java/com/flab/oasis/OasisApplication.java
@@ -0,0 +1,13 @@
+package com.flab.oasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisApplication.class, args);
+	}
+
+}
