6e988339de9aa59d5f9050553015468c04799227
dev-msj <msj944@naver.com>
2023-01-19 15:58:30 +0900

    refactor : Book 객체를 비교할 수 있도록 메서드 추가
    Override Method
    - hashCode
    - equals

--- src/main/java/com/flab/oasis/model/Book.java
+++ src/main/java/com/flab/oasis/model/Book.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -17,4 +18,28 @@ public class Book {
     private String imageUrl;
     private String description;
     private String categoryId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getBookId(), book.getBookId())
+                && Objects.equals(getTitle(), book.getTitle())
+                && Objects.equals(getAuthor(), book.getAuthor())
+                && Objects.equals(getTranslator(), book.getTranslator())
+                && Objects.equals(getPublisher(), book.getPublisher())
+                && Objects.equals(getPublishDate(), book.getPublishDate())
+                && Objects.equals(getImageUrl(), book.getImageUrl())
+                && Objects.equals(getDescription(), book.getDescription())
+                && Objects.equals(getCategoryId(), book.getCategoryId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                getBookId(), getTitle(), getAuthor(), getTranslator(), getPublisher(),
+                getPublishDate(), getImageUrl(), getDescription(), getCategoryId()
+        );
+    }
 }
