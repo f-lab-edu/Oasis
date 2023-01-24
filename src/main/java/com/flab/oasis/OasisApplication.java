f94894b7e04fd432d89300c9d1dd390c17b0a66a
dev-msj <msj944@naver.com>
2023-01-24 10:19:31 +0900

    chore : Spring Security 비활성화 및 Mapper 경로 수정

--- src/main/java/com/flab/oasis/OasisApplication.java
+++ src/main/java/com/flab/oasis/OasisApplication.java
@@ -2,10 +2,11 @@ package com.flab.oasis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class OasisApplication {
 
 	public static void main(String[] args) {
