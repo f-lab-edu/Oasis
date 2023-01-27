a7b292a29ee79ce6a988c8900a188e6c5b391060
dev-msj <msj944@naver.com>
2023-01-28 03:08:42 +0900

    test : SuggestionType Enum 테스트 코드 추가

---
+++ src/test/java/com/flab/oasis/home/SuggestionTypeTest.java
@@ -0,0 +1,14 @@
+package com.flab.oasis.home;
+
+import com.flab.oasis.service.home.SuggestionType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SuggestionTypeTest {
+    @Test
+    void suggestionTypeTest() {
+        String type = "bestSeller";
+        SuggestionType suggestionType = SuggestionType.valueOf(type.toUpperCase());
+        Assertions.assertEquals(type, suggestionType.getSuggestionType());
+    }
+}
