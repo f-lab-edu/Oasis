4d6245f7b8332129f202cf49dc11e1755ef25745
dev-msj <msj944@naver.com>
2023-01-27 15:56:17 +0900

    style : 사용되지 않는 객체 및 import 삭제

--- src/main/java/com/flab/oasis/controller/home/HomeController.java
+++ src/main/java/com/flab/oasis/controller/home/HomeController.java
@@ -3,16 +3,11 @@ package com.flab.oasis.controller.home;
 import com.flab.oasis.model.Book;
 import com.flab.oasis.service.home.HomeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -21,9 +16,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class HomeController {
     private final HomeService homeService;
-
-    @Resource
-    CacheManager cacheManager;
 
     @PostMapping("/suggestion")
     public List<Book> bookSuggestion(@RequestBody Map<String, String> map) {
