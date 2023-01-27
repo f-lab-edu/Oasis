3be1dd7040dc25a917bedc1bad4f438390df00ba
dev-msj <msj944@naver.com>
2023-01-27 16:02:36 +0900

    refactor : JsonUtils.parseValueToBookList를 BookSuggestParser로 이동

--- src/main/java/com/flab/oasis/utils/JsonUtils.java
+++ src/main/java/com/flab/oasis/utils/JsonUtils.java
@@ -1,17 +1,13 @@
 package com.flab.oasis.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.flab.oasis.model.Book;
-
-import java.util.List;
 
 public class JsonUtils {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static String parseValueToString(Object value) {
+    public static String parseObjectToString(Object value) {
         try {
             return objectMapper.writeValueAsString(value);
         } catch (JsonProcessingException e) {
@@ -19,15 +15,11 @@ public class JsonUtils {
         }
     }
 
-    public static JsonNode parseValueToJsonArray(String value) {
+    public static JsonNode parseStringToJsonNode(String value) {
         try {
             return objectMapper.readTree(value);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static List<Book> parseValueToBookList(JsonNode value) {
-        return objectMapper.convertValue(value, new TypeReference<List<Book>>() {});
     }
 }
