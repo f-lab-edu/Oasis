53b3793fe526e3090840c48c09ac499ffd9d303a
dev-msj <msj944@naver.com>
2023-02-02 01:30:05 +0900

    refactor : HomeMapper의 List 반환값들을 일급 컬렉션으로 wrapping
    일급 컬렉션 안에서 관련 비즈니스 로직들이 처리되도록 변경

---
+++ src/main/java/com/flab/oasis/model/home/BookSuggestionCollection.java
@@ -0,0 +1,44 @@
+package com.flab.oasis.model.home;
+
+import com.flab.oasis.constant.SuggestionType;
+import com.flab.oasis.model.Book;
+import com.flab.oasis.utils.JsonUtils;
+import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class BookSuggestionCollection {
+    private final List<BookSuggestion> bookSuggestionList;
+
+    public Map<String, String> parseToHashFieldValue() {
+        Map<String, String> hashFieldValue = new HashMap<>();
+        for (Map.Entry<SuggestionType, List<BookSuggestion>> entry : groupBySuggestionType().entrySet()) {
+            hashFieldValue.put(
+                    getSuggestionType(entry),
+                    JsonUtils.parseObjectToString(groupByCategoryId(getBookSuggestionList(entry)))
+            );
+        }
+
+        return hashFieldValue;
+    }
+
+    private Map<SuggestionType, List<BookSuggestion>> groupBySuggestionType() {
+        return bookSuggestionList.stream().collect(Collectors.groupingBy(BookSuggestion::getSuggestionType));
+    }
+
+    private String getSuggestionType(Map.Entry<SuggestionType, List<BookSuggestion>> entry) {
+        return entry.getKey().getSuggestionType();
+    }
+
+    private List<BookSuggestion> getBookSuggestionList(Map.Entry<SuggestionType, List<BookSuggestion>> entry) {
+        return entry.getValue();
+    }
+
+    private Map<Integer, List<Book>> groupByCategoryId(List<BookSuggestion> bookSuggestionListBySuggestionType) {
+        return bookSuggestionListBySuggestionType.stream().collect(Collectors.groupingBy(Book::getCategoryId));
+    }
+}
---
+++ src/main/java/com/flab/oasis/model/home/UserCategoryCollection.java
@@ -0,0 +1,15 @@
+package com.flab.oasis.model.home;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class UserCategoryCollection {
+    private final List<UserCategory> userCategoryList;
+
+    public List<String> getCategoryIdList() {
+        return userCategoryList.stream().map(UserCategory::getCategoryId).collect(Collectors.toList());
+    }
+}
