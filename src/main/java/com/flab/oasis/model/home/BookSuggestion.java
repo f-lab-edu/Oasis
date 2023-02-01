a2077242dab988055992a2b3aace389e33b41b70
dev-msj <msj944@naver.com>
2023-02-02 01:36:31 +0900

    refactor : BookSuggestion에서suggestionType의 타입을 String에서 SuggestionType으로 변경

--- src/main/java/com/flab/oasis/model/home/BookSuggestion.java
+++ src/main/java/com/flab/oasis/model/home/BookSuggestion.java
@@ -1,15 +1,18 @@
 package com.flab.oasis.model.home;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flab.oasis.constant.SuggestionType;
 import com.flab.oasis.model.Book;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class BookSuggestion extends Book {
     @JsonIgnore
-    private String suggestionType;
+    private SuggestionType suggestionType;
 }
