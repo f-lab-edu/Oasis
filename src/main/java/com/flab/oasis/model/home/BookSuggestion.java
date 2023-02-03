ce12f07c9d82eb4d6de978a3e4b9abb136223080
dev-msj <msj944@naver.com>
2023-02-04 05:42:23 +0900

    fix : 쿼리 결과가 enum에 매칭되지 않는 문제 해결

--- src/main/java/com/flab/oasis/model/home/BookSuggestion.java
+++ src/main/java/com/flab/oasis/model/home/BookSuggestion.java
@@ -5,14 +5,16 @@ import com.flab.oasis.constant.SuggestionType;
 import com.flab.oasis.model.Book;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class BookSuggestion extends Book {
     @JsonIgnore
     private SuggestionType suggestionType;
+
+    public BookSuggestion(String suggestionType) {
+        this.suggestionType = SuggestionType.valueOf(suggestionType);
+    }
 }
