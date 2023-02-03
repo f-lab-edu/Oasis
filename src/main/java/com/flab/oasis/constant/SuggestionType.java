114235ffff7be3eaed178ed5ca031ef00c1f3a24
dev-msj <msj944@naver.com>
2023-02-04 05:43:11 +0900

    refactor : SuggestionType 관련 데이터는 대문자로 활용하도록 변경

--- src/main/java/com/flab/oasis/constant/SuggestionType.java
+++ src/main/java/com/flab/oasis/constant/SuggestionType.java
@@ -4,13 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum SuggestionType {
-    RECOMMEND("recommend"),
-    NEWBOOK("newBook"),
-    BESTSELLER("bestSeller");
-
-    private final String suggestionType;
-
-    SuggestionType(String suggestionType) {
-        this.suggestionType = suggestionType;
-    }
+    RECOMMEND,
+    NEWBOOK,
+    BESTSELLER;
 }
