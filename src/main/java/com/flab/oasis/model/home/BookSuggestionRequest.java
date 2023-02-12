cb7997aaddb99de373928cf194ee071c26b6de14
dev-msj <msj944@naver.com>
2023-02-12 20:14:04 +0900

    refactor : 통신이 되는 객체이므로 Serializable 상속

--- src/main/java/com/flab/oasis/model/home/BookSuggestionRequest.java
+++ src/main/java/com/flab/oasis/model/home/BookSuggestionRequest.java
@@ -4,9 +4,11 @@ import com.flab.oasis.constant.SuggestionType;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class BookSuggestionRequest {
+public class BookSuggestionRequest implements Serializable {
     private String uid;
     private SuggestionType suggestionType;
 }
