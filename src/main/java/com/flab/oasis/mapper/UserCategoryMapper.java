b29e1e27461d8c9c9b3d1cb5768e221f9eeaca4a
dev-msj <msj944@naver.com>
2023-02-17 02:55:01 +0900

    fix : 현재 feature와 상관없는 코드 삭제

--- src/main/java/com/flab/oasis/mapper/UserCategoryMapper.java
+++ src/main/java/com/flab/oasis/mapper/UserCategoryMapper.java
@@ -7,7 +7,5 @@ import java.util.List;
 
 @Mapper
 public interface UserCategoryMapper {
-    public void insertUserCategory(UserCategory userCategory);
     public List<UserCategory> findUserCategoryByUid(String uid);
-    public void deleteUserCategoryByUid(String uid);
 }
