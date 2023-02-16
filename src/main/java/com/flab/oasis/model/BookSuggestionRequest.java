afff9d1f0eb91e744c0f927f0e5a70331e3eb155
dev-msj <msj944@naver.com>
2023-02-17 01:09:41 +0900

    style : BookSuggestionRequest에 AllArgsConstructor 추가

--- src/main/java/com/flab/oasis/model/BookSuggestionRequest.java
+++ src/main/java/com/flab/oasis/model/BookSuggestionRequest.java
@@ -1,16 +1,14 @@
 package com.flab.oasis.model;
 
 import com.flab.oasis.constant.SuggestionType;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class BookSuggestionRequest implements BaseRequest, Serializable {
     private static final long serialVersionUID = -755154959612591958L;
