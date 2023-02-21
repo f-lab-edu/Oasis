32115f60a20e7aab7746550e5e309dea416b5475
dev-msj <msj944@naver.com>
2023-02-22 04:03:32 +0900

    style(TokenInterceptor) : 토큰 인터셉터 처리 로직 설계

--- src/main/java/com/flab/oasis/interceptor/TokenInterceptor.java
+++ src/main/java/com/flab/oasis/interceptor/TokenInterceptor.java
@@ -1,5 +1,7 @@
 package com.flab.oasis.interceptor;
 
+import com.flab.oasis.service.JwtService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -7,12 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
+@RequiredArgsConstructor
 public class TokenInterceptor implements HandlerInterceptor {
+    private final JwtService jwtService;
+
     @Override
     public boolean preHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (request.getMethod().equals("POST")) {
-            authJwtToken(request, response, handler);
+            return authJwtToken(request, response, handler);
         }
 
         return true;
@@ -20,7 +25,13 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     private boolean authJwtToken(
             HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // jwt 인증 프로세스 호출
-        return true;
+        // Access Token이 유효한지 검사한다. -> jwtService.validateToken() 호출
+        // jwtService.validateToken()의 결과가 true면 Access Token이 만료 되었는지 검사한다. -> jwtService.isExpired() 호출
+        // jwtService.isExpired()의 결과가 NOT_EXPIRED면 true를 반환한다.
+        // jwtService.isExpired()의 결과가 EXPIRED면 false를 반환한다.
+
+        // AuthorizationException을 catch하면 err message와 err code를 response한다.
+
+        return false;
     }
 }
