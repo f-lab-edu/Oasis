e2e5a5e2cbdeaeec1c7f03ab88ca69d52c3eb24d
dev-msj <msj944@naver.com>
2023-02-22 03:56:09 +0900

    refactor(UserLoginRequest) : Serializable 상속

--- src/main/java/com/flab/oasis/model/UserLoginRequest.java
+++ src/main/java/com/flab/oasis/model/UserLoginRequest.java
@@ -3,9 +3,13 @@ package com.flab.oasis.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class UserLoginRequest implements BaseRequest {
+public class UserLoginRequest implements BaseRequest, Serializable {
+    private static final long serialVersionUID = 4305118098455201936L;
+
     private String uid;
     private String password;
 
