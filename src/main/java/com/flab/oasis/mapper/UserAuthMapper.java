0e55a5e6ceae51b14c11712aa49e02a447163f44
dev-msj <msj944@naver.com>
2023-02-21 19:23:59 +0900

    feat : UserAuthMapper 구현

---
+++ src/main/java/com/flab/oasis/mapper/UserAuthMapper.java
@@ -0,0 +1,10 @@
+package com.flab.oasis.mapper;
+
+import com.flab.oasis.model.UserAuth;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserAuthMapper {
+    public UserAuth getUserAuthByUid(String uid);
+    public void updateUserAuth(UserAuth userAuth);
+}
