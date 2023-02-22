aec58bca4d50b7eb7caa9d608c28e5fdd8aeb35e
dev-msj <msj944@naver.com>
2023-02-23 01:54:37 +0900

    feat : TokenInterceptor 구현

--- src/main/java/com/flab/oasis/interceptor/TokenInterceptor.java
+++ src/main/java/com/flab/oasis/interceptor/TokenInterceptor.java
@@ -1,5 +1,9 @@
 package com.flab.oasis.interceptor;
 
+import com.auth0.jwt.exceptions.InvalidClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.flab.oasis.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -25,12 +29,21 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     private boolean authJwtToken(
             HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // Access Token이 유효한지 검사한다. -> jwtService.validateToken() 호출
-        // jwtService.validateToken()의 결과가 true면 Access Token이 만료 되었는지 검사한다. -> jwtService.isExpired() 호출
-        // jwtService.isExpired()의 결과가 NOT_EXPIRED면 true를 반환한다.
-        // jwtService.isExpired()의 결과가 EXPIRED면 false를 반환한다.
+        String jwtHeader = request.getHeader("authorization");
+        if (jwtHeader != null && jwtHeader.startsWith("Bearer ")) {
+            try {
+                DecodedJWT decodedJWT = jwtService.decodeJWT(jwtHeader.substring(7));
+                if (!jwtService.checkTokenInfoExistInDB(decodedJWT)) {
+                    response.sendError(404, "Access Token ID doesn't exist in DB.");
+                }
 
-        // AuthorizationException을 catch하면 err message와 err code를 response한다.
+                return true;
+            } catch (TokenExpiredException e) {
+                response.sendError(205, "Access Token is Expired.");
+            } catch (SignatureVerificationException | InvalidClaimException e) {
+                response.sendError(401, "Invalid Refresh Token.");
+            }
+        }
 
         return false;
     }
