dc2a48b3ed4eec171712365d0a676bc3b3e84d2f
dev-msj <msj944@naver.com>
2023-02-23 04:25:55 +0900

    refactor(HomeController) : api prefix 적용

--- src/main/java/com/flab/oasis/controller/HomeController.java
+++ src/main/java/com/flab/oasis/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api/home")
 @RequiredArgsConstructor
 public class HomeController {
     private final HomeService homeService;
