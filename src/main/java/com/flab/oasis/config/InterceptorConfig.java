05df618c74ea9245ed507655263ba6d9d5489db7
dev-msj <msj944@naver.com>
2023-02-23 04:24:09 +0900

    refactor : api prefix 적용
    InterceptorConfig, UserAuthController의 request mapping 주소에 api prefix 적용

--- src/main/java/com/flab/oasis/config/InterceptorConfig.java
+++ src/main/java/com/flab/oasis/config/InterceptorConfig.java
@@ -15,6 +15,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
                 .addPathPatterns("/*")
-                .excludePathPatterns("/auth/refresh");
+                .excludePathPatterns("/api/auth/refresh");
     }
 }
--- src/main/java/com/flab/oasis/controller/UserAuthController.java
+++ src/main/java/com/flab/oasis/controller/UserAuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UserAuthController {
     private final UserAuthService userAuthService;
