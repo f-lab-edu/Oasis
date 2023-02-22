5708cacf9889c7f1fc5554dd65ce024c530146ee
dev-msj <msj944@naver.com>
2023-02-23 04:22:52 +0900

    refactor(InterceptorConfig) : refresh 요청은 interceptor 제외

--- src/main/java/com/flab/oasis/config/InterceptorConfig.java
+++ src/main/java/com/flab/oasis/config/InterceptorConfig.java
@@ -13,6 +13,8 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor).addPathPatterns("/*");
+        registry.addInterceptor(tokenInterceptor)
+                .addPathPatterns("/*")
+                .excludePathPatterns("/auth/refresh");
     }
 }
