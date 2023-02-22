f3c5b80e019a29779d2c3856cd5964b1bfadf7ef
dev-msj <msj944@naver.com>
2023-02-23 01:40:35 +0900

    feat : UserAuthService 구현

--- src/main/java/com/flab/oasis/service/UserAuthService.java
+++ src/main/java/com/flab/oasis/service/UserAuthService.java
@@ -4,8 +4,13 @@ import com.flab.oasis.mapper.UserAuthMapper;
 import com.flab.oasis.model.JwtToken;
 import com.flab.oasis.model.UserAuth;
 import com.flab.oasis.model.UserLoginRequest;
+import com.flab.oasis.model.exception.AuthorizationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -14,22 +19,39 @@ public class UserAuthService {
     private final JwtService jwtService;
 
     public JwtToken loginAuthFromUserLoginRequest(UserLoginRequest userLoginRequest) {
-        // password를 hashing한다. -> hashingPassword() 호출
-        // db에서 uid에 해당하는 userAuth를 가져온다.
-        // hashing된 password와 db의 password가 동일한지 확인한다.
-        // password가 동일하면 token을 발급한다. -> jwtService.createJwtToken() 호출
-        // password가 동일하지 않으면 AuthorizationException을 던진다.
-        return null;
+        String salt = String.valueOf(new Date().getTime());
+        String hashingPassword = hashingPassword(userLoginRequest.getPassword(), salt);
+        UserAuth userAuth = userAuthMapper.getUserAuthByUid(userLoginRequest.getUid());
+
+        if (!userAuth.getPassword().equals(hashingPassword)) {
+            throw new AuthorizationException("Password doesn't match.");
+        }
+
+        return jwtService.createJwtToken(userAuth.getUid());
     }
 
-    public void updateRefreshToken(UserAuth userAuth) {
-        userAuthMapper.updateRefreshToken(userAuth);
+    private String hashingPassword(String password, String salt) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+
+            // password에 salt 더하여 key-stretch를 3회 반복한다.
+            for (int i = 0; i < 3; i++) {
+                String passwordSalt = password + salt;
+                messageDigest.update(passwordSalt.getBytes());
+                password = byteToString(messageDigest.digest());
+            }
+
+            return password;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
-    private String hashingPassword(String password) {
-        // key-stretch와 salt를 활용하여 패스워드 해싱
-        // key-stretch : 3회
-        // salt : timestamp
-        return null;
+    private String byteToString(byte[] temp) {
+        StringBuilder sb = new StringBuilder();
+        for(byte a : temp) {
+            sb.append(String.format("%02x", a));
+        }
+        return sb.toString();
     }
 }
