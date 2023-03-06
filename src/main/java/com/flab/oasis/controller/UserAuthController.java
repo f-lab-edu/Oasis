20aec1286ed7d052c741b8772908d6c9daf1af6e
dev-msj <msj944@naver.com>
2023-03-07 04:18:00 +0900

    refactor(UserAuthController) : 토큰 반환 방식 변경
    access token을 cookie에 담고 refresh token을 header에 담도록 변경

--- src/main/java/com/flab/oasis/controller/UserAuthController.java
+++ src/main/java/com/flab/oasis/controller/UserAuthController.java
@@ -3,15 +3,16 @@ package com.flab.oasis.controller;
 import com.flab.oasis.constant.JwtProperty;
 import com.flab.oasis.model.JwtToken;
 import com.flab.oasis.model.UserLoginRequest;
+import com.flab.oasis.model.exception.AuthorizationException;
 import com.flab.oasis.service.JwtService;
 import com.flab.oasis.service.UserAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -21,15 +22,23 @@ public class UserAuthController {
     private final UserAuthService userAuthService;
     private final JwtService jwtService;
 
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
     @PostMapping("/refresh")
     public HttpServletResponse reissueJwtToken(
-            @CookieValue("refreshToken") String refreshToken, HttpServletResponse response) {
-        JwtToken jwtToken = jwtService.reissueJwtToken(refreshToken);
+            HttpServletRequest request, HttpServletResponse response) {
+        String jwtHeader = request.getHeader(AUTHORIZATION_HEADER);
+        if (jwtHeader != null && jwtHeader.startsWith("Bearer ")) {
+            String refreshToken = jwtHeader.substring(7);
+            JwtToken jwtToken = jwtService.reissueJwtToken(refreshToken);
 
-        response.setHeader("Authorization", String.format("%s %s", "Bearer", jwtToken.getAccessToken()));
-        response.addCookie(createCookie(jwtToken.getRefreshToken()));
+            response.addCookie(createCookie(jwtToken.getAccessToken()));
+            response.setHeader(AUTHORIZATION_HEADER, String.format("%s %s", "Bearer", jwtToken.getRefreshToken()));
 
-        return response;
+            return response;
+        }
+
+        throw new AuthorizationException("Refresh Token does not exist.");
     }
 
     @PostMapping("/login/default")
@@ -37,15 +46,16 @@ public class UserAuthController {
             UserLoginRequest userLoginRequest, HttpServletResponse response) {
         JwtToken jwtToken = userAuthService.loginAuthFromUserLoginRequest(userLoginRequest);
 
-        response.setHeader("Authorization", String.format("%s %s", "Bearer", jwtToken.getAccessToken()));
-        response.addCookie(createCookie(jwtToken.getRefreshToken()));
+        response.addCookie(createCookie(jwtToken.getAccessToken()));
+        response.setHeader(AUTHORIZATION_HEADER, String.format("%s %s", "Bearer", jwtToken.getRefreshToken()));
 
         return response;
     }
 
-    private static Cookie createCookie(String refreshToken) {
-        Cookie cookie = new Cookie("RefreshToken", refreshToken);
+    private static Cookie createCookie(String accessToken) {
+        Cookie cookie = new Cookie("token", accessToken);
         cookie.setMaxAge(JwtProperty.REFRESH_TOKEN_EXPIRE_TIME / 1000); // 초 단위로 변경
+        cookie.setHttpOnly(true);
 
         return cookie;
     }
