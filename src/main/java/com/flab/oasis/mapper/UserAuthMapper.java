515ab1dbf89b2ea45aab26b924f97514f924d16a
dev-msj <msj944@naver.com>
2023-03-07 04:20:07 +0900

    style(UserAuthMapper) : 사용되지 않는 쿼리 삭제

--- src/main/java/com/flab/oasis/mapper/UserAuthMapper.java
+++ src/main/java/com/flab/oasis/mapper/UserAuthMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserAuthMapper {
     public UserAuth getUserAuthByUid(String uid);
-    public boolean existUserAuthByUid(String uid);
     public boolean existUserAuthByUidAndRefreshToken(UserAuth userAuth);
     public void updateRefreshToken(UserAuth userAuth);
 }
