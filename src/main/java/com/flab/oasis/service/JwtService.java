34dbc9aada067291b09b6d87640f1fd221fbf3a9
dev-msj <msj944@naver.com>
2023-03-07 19:25:22 +0900

    refactor(JwtService) : redis에서 refresh token을 관리하도록 변경

--- src/main/java/com/flab/oasis/service/JwtService.java
+++ src/main/java/com/flab/oasis/service/JwtService.java
@@ -7,11 +7,11 @@ import com.auth0.jwt.exceptions.InvalidClaimException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.flab.oasis.constant.ErrorCode;
 import com.flab.oasis.constant.JwtProperty;
-import com.flab.oasis.mapper.UserAuthMapper;
 import com.flab.oasis.model.JwtToken;
-import com.flab.oasis.model.UserAuth;
 import com.flab.oasis.model.exception.AuthorizationException;
+import com.flab.oasis.repository.UserAuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +20,7 @@ import java.util.Date;
 @Service
 @RequiredArgsConstructor
 public class JwtService {
-    private final UserAuthMapper userAuthMapper;
+    private final UserAuthRepository userAuthRepository;
 
     private static final String ACCESS_TOKEN = "AccessToken";
     private static final String REFRESH_TOKEN = "RefreshToken";
@@ -29,9 +29,7 @@ public class JwtService {
         String accessToken = generateToken(uid, ACCESS_TOKEN);
         String refreshToken = generateToken(uid, REFRESH_TOKEN);
 
-        userAuthMapper.updateRefreshToken(
-                UserAuth.builder().uid(uid).refreshToken(refreshToken).build()
-        );
+        userAuthRepository.setUserRefreshToken(uid, refreshToken);
 
         return new JwtToken(accessToken, refreshToken);
     }
@@ -40,9 +38,10 @@ public class JwtService {
         try {
             DecodedJWT decodedJWT = verifyJwt(refreshToken);
 
-            if (!checkTokenInfoExistInDB(decodedJWT)) {
-                System.out.printf("Refresh Token doesn't exist in DB - %s", refreshToken);
-                throw new AuthorizationException("Refresh Token doesn't exist in DB.");
+            // refresh token이 redis에 존재하지 않으면 다시 로그인을 하도록 한다.
+            if (!userAuthRepository.existUserRefreshToken(decodedJWT.getSubject(), refreshToken)) {
+                System.out.printf("Refresh Token doesn't exist in DB - %s%n", refreshToken);
+                throw new AuthorizationException("Refresh Token doesn't exist in redis.", ErrorCode.NOT_FOUND);
             }
 
             // refresh token 만료 1일 전이면 토큰을 새로 발급한다.
@@ -55,11 +54,11 @@ public class JwtService {
                     generateToken(decodedJWT.getSubject(), ACCESS_TOKEN), refreshToken
             );
         } catch (TokenExpiredException e) {
-            System.out.printf("Refresh Token is Expired - %s\n", refreshToken);
-            throw new AuthorizationException("Refresh Token is Expired.");
+            System.out.printf("Refresh Token is Expired - %s%n", refreshToken);
+            throw new AuthorizationException("Refresh Token is Expired.", ErrorCode.UNAUTHORIZED);
         } catch (SignatureVerificationException | InvalidClaimException e) {
-            System.out.printf("Invalid Refresh Token. - %s\n", refreshToken);
-            throw new AuthorizationException("Invalid Refresh Token.");
+            System.out.printf("Invalid Refresh Token. - %s%n", refreshToken);
+            throw new AuthorizationException("Invalid Refresh Token.", ErrorCode.UNAUTHORIZED);
         }
     }
 
@@ -71,15 +70,6 @@ public class JwtService {
                 .build();
 
         return verifier.verify(token);
-    }
-
-    private boolean checkTokenInfoExistInDB(DecodedJWT decodedJWT) throws InvalidClaimException {
-        return userAuthMapper.existUserAuthByUidAndRefreshToken(
-                UserAuth.builder()
-                        .uid(decodedJWT.getClaim("uid").asString())
-                        .refreshToken(decodedJWT.getToken())
-                        .build()
-        );
     }
 
     private String generateToken(String uid, String tokenType) {
