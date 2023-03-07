c53267f04baf2c0bdf22c81022f1d76b245dc9b0
dev-msj <msj944@naver.com>
2023-03-07 19:20:00 +0900

    refactor(AuthorizationException) : 에러 코드를 같이 받도록 변경

--- src/main/java/com/flab/oasis/model/exception/AuthorizationException.java
+++ src/main/java/com/flab/oasis/model/exception/AuthorizationException.java
@@ -1,9 +1,16 @@
 package com.flab.oasis.model.exception;
 
+import com.flab.oasis.constant.ErrorCode;
+import lombok.Getter;
+
+@Getter
 public class AuthorizationException extends RuntimeException {
     private static final long serialVersionUID = -7138577059223481163L;
 
-    public AuthorizationException(String message) {
+    private final ErrorCode errorCode;
+
+    public AuthorizationException(String message, ErrorCode errorCode) {
         super(message);
+        this.errorCode = errorCode;
     }
 }
