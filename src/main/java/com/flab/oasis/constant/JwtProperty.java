5b735e114654761c4f8c404bddc121f71ed765d2
dev-msj <msj944@naver.com>
2023-03-07 19:18:32 +0900

    style(JwtProperty) : 토큰 만료 시간 변경

--- src/main/java/com/flab/oasis/constant/JwtProperty.java
+++ src/main/java/com/flab/oasis/constant/JwtProperty.java
@@ -4,7 +4,7 @@ package com.flab.oasis.constant;
 public class JwtProperty {
     public static final String SECRET_KEY = "OasisAuth";
     public static final String ISSUER = "OasisServer";
-    public static final int ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; // 1일
-    public static final int REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 1주일
-    public static final int REFRESH_TOKEN_REISSUE_TIME = 1000 * 60 * 60 * 24; // 1일
+    public static final int ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1시간
+    public static final int REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14; // 2주일
+    public static final int REFRESH_TOKEN_REISSUE_TIME = 1000 * 60 * 60 * 24 * 3; // 3일
 }
