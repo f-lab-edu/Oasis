f19f9db808f45dc2fe3f77ed09b9ab5cbbb64c61
dev-msj <msj944@naver.com>
2023-03-08 05:16:38 +0900

    refactor(UserAuthController) : refresh token을 쿠키에 담도록 변경

--- src/main/java/com/flab/oasis/controller/UserAuthController.java
+++ src/main/java/com/flab/oasis/controller/UserAuthController.java
@@ -3,16 +3,12 @@ package com.flab.oasis.controller;
 import com.flab.oasis.constant.JwtProperty;
 import com.flab.oasis.model.JwtToken;
 import com.flab.oasis.model.UserLoginRequest;
-import com.flab.oasis.model.exception.AuthorizationException;
 import com.flab.oasis.service.JwtService;
 import com.flab.oasis.service.UserAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -23,40 +19,40 @@ public class UserAuthController {
     private final JwtService jwtService;
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String SET_COOKIE = "Set-Cookie";
 
     @PostMapping("/refresh")
-    public HttpServletResponse reissueJwtToken(
-            HttpServletRequest request, HttpServletResponse response) {
-        String jwtHeader = request.getHeader(AUTHORIZATION_HEADER);
-        if (jwtHeader != null && jwtHeader.startsWith("Bearer ")) {
-            String refreshToken = jwtHeader.substring(7);
-            JwtToken jwtToken = jwtService.reissueJwtToken(refreshToken);
+    public boolean reissueJwtToken(@CookieValue("RefreshToken") String refreshToken, HttpServletResponse response) {
+        JwtToken jwtToken = jwtService.reissueJwtToken(refreshToken);
 
-            response.addCookie(createCookie(jwtToken.getAccessToken()));
-            response.setHeader(AUTHORIZATION_HEADER, String.format("%s %s", "Bearer", jwtToken.getRefreshToken()));
+        response.setHeader(AUTHORIZATION_HEADER, makeAuthorizationValue(jwtToken.getAccessToken()));
+        response.setHeader(SET_COOKIE, createCookie(jwtToken.getRefreshToken()));
 
-            return response;
-        }
-
-        throw new AuthorizationException("Refresh Token does not exist.");
+        return true;
     }
 
     @PostMapping("/login/default")
-    public HttpServletResponse loginAuthFromUserLoginRequest(
-            UserLoginRequest userLoginRequest, HttpServletResponse response) {
-        JwtToken jwtToken = userAuthService.loginAuthFromUserLoginRequest(userLoginRequest);
+    public boolean loginAuthFromUserLoginRequest(
+            @RequestBody UserLoginRequest userLoginRequest, HttpServletResponse response) {
+        JwtToken jwtToken = userAuthService.createJwtTokenByUserLoginRequest(userLoginRequest);
 
-        response.addCookie(createCookie(jwtToken.getAccessToken()));
-        response.setHeader(AUTHORIZATION_HEADER, String.format("%s %s", "Bearer", jwtToken.getRefreshToken()));
+        response.setHeader(AUTHORIZATION_HEADER, makeAuthorizationValue(jwtToken.getAccessToken()));
+        response.setHeader(SET_COOKIE, createCookie(jwtToken.getRefreshToken()));
 
-        return response;
+        return true;
     }
 
-    private static Cookie createCookie(String accessToken) {
-        Cookie cookie = new Cookie("token", accessToken);
-        cookie.setMaxAge(JwtProperty.REFRESH_TOKEN_EXPIRE_TIME / 1000); // 초 단위로 변경
-        cookie.setHttpOnly(true);
+    private String makeAuthorizationValue(String accessToken) {
+        return String.format("%s %s", "Bearer", accessToken);
+    }
 
-        return cookie;
+    private String createCookie(String refreshToken) {
+        return ResponseCookie.from("RefreshToken", refreshToken)
+                .maxAge(JwtProperty.REFRESH_TOKEN_EXPIRE_TIME / 1000)
+                .httpOnly(true)
+                .path("/")
+                .sameSite("Lax")
+                .build()
+                .toString();
     }
 }
