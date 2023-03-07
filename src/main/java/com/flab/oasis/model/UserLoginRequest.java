6fb979d92095685b8eb8f5df14861511f03f158c
dev-msj <msj944@naver.com>
2023-03-08 06:49:47 +0900

    style(UserLoginRequest) : 생성자 관련 어노테이션 추가

--- src/main/java/com/flab/oasis/model/UserLoginRequest.java
+++ src/main/java/com/flab/oasis/model/UserLoginRequest.java
@@ -1,12 +1,16 @@
 package com.flab.oasis.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserLoginRequest implements BaseRequest, Serializable {
     private static final long serialVersionUID = 4305118098455201936L;
 
