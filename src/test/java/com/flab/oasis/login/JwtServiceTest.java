7c87f60e74c2e79f16d5c134ac568879f8d7a2a5
dev-msj <msj944@naver.com>
2023-03-08 06:45:55 +0900

    test(JwtServiceTest) : 테스트 코드 재작성 및 목적과 맞지 않는 테스트 삭제

--- src/test/java/com/flab/oasis/login/JwtServiceTest.java
+++ src/test/java/com/flab/oasis/login/JwtServiceTest.java
@@ -1,27 +1,17 @@
 package com.flab.oasis.login;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.InvalidClaimException;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.flab.oasis.constant.JwtProperty;
-import com.flab.oasis.mapper.UserAuthMapper;
-import com.flab.oasis.model.JwtToken;
-import com.flab.oasis.model.UserAuth;
 import com.flab.oasis.model.exception.AuthorizationException;
+import com.flab.oasis.repository.UserAuthRepository;
 import com.flab.oasis.service.JwtService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
 class JwtServiceTest {
@@ -29,80 +19,49 @@ class JwtServiceTest {
     JwtService jwtService;
 
     @Mock
-    UserAuthMapper userAuthMapper;
+    UserAuthRepository userAuthRepository;
 
     @DisplayName("토큰 신규 발급")
     @Test
     void testCreateToken() {
-        Mockito.doNothing()
-                .when(userAuthMapper)
-                .updateRefreshToken(ArgumentMatchers.any(UserAuth.class));
+        String originUid = "uid";
+        String accessToken = jwtService.createJwtToken(originUid).getAccessToken();
+        String actualUid = jwtService.verifyJwt(accessToken).getSubject();
 
-        Assertions.assertNotNull(jwtService.createJwtToken("test").getAccessToken());
+        Assertions.assertEquals(originUid, actualUid);
     }
 
-    @DisplayName("Access Token 재발급")
+    @DisplayName("Refresh Token으로 Access Token 재발급")
     @Test
-    void testReissueAccessToken() throws InterruptedException {
-        JwtToken jwtToken = jwtService.createJwtToken("test");
+    void testReissueAccessToken() {
+        String originUid = "test";
+        String refreshToken = jwtService.createJwtToken(originUid).getRefreshToken();
 
         Mockito.doReturn(true)
-                .when(userAuthMapper)
-                .existUserAuthByUidAndRefreshToken(ArgumentMatchers.any(UserAuth.class));
+                .when(userAuthRepository)
+                .existUserRefreshToken(originUid, refreshToken);
 
-        Thread.sleep(1000);
+        BDDMockito.given(userAuthRepository.existUserRefreshToken(originUid, refreshToken))
+                .willReturn(true);
 
-        Assertions.assertNotEquals(
-                jwtToken.getAccessToken(),
-                jwtService.reissueJwtToken(jwtToken.getRefreshToken()).getAccessToken()
-        );
+        String accessToken = jwtService.reissueJwtToken(refreshToken).getAccessToken();
+        String actualUid = jwtService.verifyJwt(accessToken).getSubject();
+
+        // refresh token으로 재발급된 access token 검증
+        Assertions.assertEquals(originUid, actualUid);
     }
 
     @DisplayName("Refresh Token이 DB에 존재하지 않을 경우")
     @Test
     void testReissueTokenThrowError() {
-        String refreshToken = jwtService.createJwtToken("test").getRefreshToken();
+        String uid = "test";
+        String refreshToken = jwtService.createJwtToken(uid).getRefreshToken();
 
         Mockito.doReturn(false)
-                .when(userAuthMapper)
-                .existUserAuthByUidAndRefreshToken(ArgumentMatchers.any(UserAuth.class));
+                .when(userAuthRepository)
+                .existUserRefreshToken(uid, refreshToken);
 
         Assertions.assertThrows(AuthorizationException.class, () -> jwtService.reissueJwtToken(refreshToken));
     }
 
-    @DisplayName("Issuer가 다를 경우")
-    @Test
-    void testDecodeJwtThrowIssuerError() {
-        Algorithm algorithm = Algorithm.HMAC256(JwtProperty.SECRET_KEY);
-        String accessToken = JWT.create()
-                .withIssuer("OtherIssuer")
-                .sign(algorithm);
-
-        Assertions.assertThrows(InvalidClaimException.class, () -> jwtService.verifyJwt(accessToken));
-    }
-
-    @DisplayName("알고리즘 Secret Key가 다를 경우")
-    @Test
-    void testDecodeJwtThrowSignatureError() {
-        Algorithm algorithm = Algorithm.HMAC256("OtherKey");
-        String accessToken = JWT.create()
-                .sign(algorithm);
-
-        Assertions.assertThrows(SignatureVerificationException.class, () -> jwtService.verifyJwt(accessToken));
-    }
-
-    @DisplayName("만료된 토큰일 경우")
-    @Test
-    void testDecodeJwtThrowExpired() {
-        Algorithm algorithm = Algorithm.HMAC256(JwtProperty.SECRET_KEY);
-        Date issueDate = new Date();
-
-        String accessToken = JWT.create()
-                .withIssuer(JwtProperty.ISSUER)
-                .withIssuedAt(issueDate)
-                .withExpiresAt(issueDate)
-                .sign(algorithm);
-
-        Assertions.assertThrows(TokenExpiredException.class, () -> jwtService.verifyJwt(accessToken));
-    }
 }
