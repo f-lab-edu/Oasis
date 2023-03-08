6a993f557379a68cb65b88368bf598a0c4e5f4dd
dev-msj <msj944@naver.com>
2023-03-09 04:20:50 +0900

    refactor(TokenInterceptor) : access token expire 예외처리 추가

--- src/main/java/com/flab/oasis/interceptor/TokenInterceptor.java
+++ src/main/java/com/flab/oasis/interceptor/TokenInterceptor.java
@@ -4,13 +4,21 @@ import com.auth0.jwt.exceptions.InvalidClaimException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.flab.oasis.constant.ErrorCode;
+import com.flab.oasis.constant.JwtProperty;
+import com.flab.oasis.model.JwtToken;
+import com.flab.oasis.model.exception.AuthorizationException;
 import com.flab.oasis.service.JwtService;
+import com.flab.oasis.utils.LogUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -21,14 +29,14 @@ public class TokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (request.getMethod().equals("POST")) {
-            return authJwtToken(request, response, handler);
+            return authJwtToken(request, response);
         }
 
         return true;
     }
 
     private boolean authJwtToken(
-            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
         String jwtHeader = request.getHeader("Authorization");
         if (jwtHeader != null && jwtHeader.startsWith("Bearer ")) {
             String accessToken = jwtHeader.substring(7);
@@ -38,14 +46,43 @@ public class TokenInterceptor implements HandlerInterceptor {
 
                 return true;
             } catch (TokenExpiredException e) {
-                System.out.printf("Access Token is Expired - %s%n", accessToken);
-                response.sendError(ErrorCode.RESET_CONTENT.getCode(), "Access Token is Expired.");
+                System.out.println(LogUtils.makeErrorLog(
+                        ErrorCode.UNAUTHORIZED, "Access Token is Expired.", accessToken
+                ));
+
+                String refreshToken = Optional.ofNullable(WebUtils.getCookie(request, "RefreshToken"))
+                        .orElseThrow(() -> new AuthorizationException(
+                                ErrorCode.UNAUTHORIZED, "Refresh Token does not exist in cookie."
+                        )).getValue();
+
+                JwtToken jwtToken = jwtService.reissueJwtToken(refreshToken);
+
+                response.setHeader("Authorization", String.format("%s %s", "Bearer", jwtToken.getAccessToken()));
+                response.setHeader("Set-Cookie", createCookie(jwtToken.getRefreshToken()));
+
+                response.sendError(
+                        ErrorCode.RESET_CONTENT.getCode(),
+                        "The token was reissued because the access token expired."
+                );
             } catch (SignatureVerificationException | InvalidClaimException e) {
-                System.out.printf("Invalid Access Token - %s%n", accessToken);
+                System.out.println(LogUtils.makeErrorLog(
+                        ErrorCode.UNAUTHORIZED, "Invalid Access Token.", accessToken
+                ));
+
                 response.sendError(ErrorCode.UNAUTHORIZED.getCode(), "Invalid Access Token.");
             }
         }
 
         return false;
+    }
+
+    private String createCookie(String refreshToken) {
+        return ResponseCookie.from("RefreshToken", refreshToken)
+                .maxAge(JwtProperty.REFRESH_TOKEN_EXPIRE_TIME / 1000)
+                .httpOnly(true)
+                .path("/")
+                .sameSite("Lax")
+                .build()
+                .toString();
     }
 }
