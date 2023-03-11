2a79098e9337f50cd098e2dc71d152cf4270fcdd
dev-msj <msj944@naver.com>
2023-03-12 00:15:51 +0900

    feat(UserAuthService) : 구글 인증 토큰을 활용한 JWT 토큰 발급 기능 구현

--- src/main/java/com/flab/oasis/service/UserAuthService.java
+++ src/main/java/com/flab/oasis/service/UserAuthService.java
@@ -4,13 +4,24 @@ import com.flab.oasis.constant.ErrorCode;
 import com.flab.oasis.mapper.UserAuthMapper;
 import com.flab.oasis.model.JwtToken;
 import com.flab.oasis.model.UserAuth;
+import com.flab.oasis.model.UserGoogleAuthToken;
 import com.flab.oasis.model.UserLoginRequest;
 import com.flab.oasis.model.exception.AuthorizationException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -18,6 +29,9 @@ import java.util.Optional;
 public class UserAuthService {
     private final UserAuthMapper userAuthMapper;
     private final JwtService jwtService;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
 
     public JwtToken createJwtTokenByUserLoginRequest(UserLoginRequest userLoginRequest) {
         UserAuth userAuth = Optional.ofNullable(userAuthMapper.getUserAuthByUid(userLoginRequest.getUid()))
@@ -33,6 +47,32 @@ public class UserAuthService {
         }
 
         return jwtService.createJwtToken(userAuth.getUid());
+    }
+
+    public JwtToken createJwtTokenByUserGoogleAuthToken(UserGoogleAuthToken userGoogleAuthToken) {
+        HttpTransport transport = new NetHttpTransport();
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                .setAudience(Collections.singletonList(googleClientId)).build();
+
+        try {
+            GoogleIdToken googleIdToken = verifier.verify(userGoogleAuthToken.getToken());
+            UserAuth userAuth = Optional.ofNullable(
+                    userAuthMapper.getUserAuthByUid(googleIdToken.getPayload().getEmail())
+                    ).orElseThrow(() -> new AuthorizationException(
+                            ErrorCode.UNAUTHORIZED, "User does not exist.", googleIdToken.getPayload().getEmail()
+                    ));
+
+            return jwtService.createJwtToken(userAuth.getUid());
+
+        } catch (GeneralSecurityException e) {
+            throw new AuthorizationException(
+                    ErrorCode.UNAUTHORIZED, "Invalid Google Auth Token.", userGoogleAuthToken.getToken()
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private String hashingPassword(String password, String salt) {
