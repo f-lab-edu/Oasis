3c53b0559e561ee2900cdd98170dd3c530d66ea6
dev-msj <msj944@naver.com>
2023-03-12 00:11:35 +0900

    feat(JwtFilter) : JWT 필터 구현

---
+++ src/main/java/com/flab/oasis/filter/JwtFilter.java
@@ -0,0 +1,83 @@
+package com.flab.oasis.filter;
+
+import com.auth0.jwt.exceptions.InvalidClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.flab.oasis.constant.ErrorCode;
+import com.flab.oasis.constant.JwtProperty;
+import com.flab.oasis.model.JwtToken;
+import com.flab.oasis.model.exception.AuthorizationException;
+import com.flab.oasis.service.JwtService;
+import com.flab.oasis.utils.LogUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    private static final String ACCESS_TOKEN = "AccessToken";
+    private static final String REFRESH_TOKEN = "RefreshToken";
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String accessToken = Optional.ofNullable(WebUtils.getCookie(request, ACCESS_TOKEN))
+                .orElseThrow(() -> new AuthorizationException(
+                        ErrorCode.UNAUTHORIZED, "Access Token does not exist in cookie.")
+                ).getValue();
+
+        try {
+            jwtService.verifyJwt(accessToken);
+
+            chain.doFilter(request, response);
+        } catch (TokenExpiredException e) {
+            System.out.println(LogUtils.makeErrorLog(
+                    ErrorCode.UNAUTHORIZED, "Access Token is Expired.", accessToken
+            ));
+
+            String refreshToken = Optional.ofNullable(WebUtils.getCookie(request, REFRESH_TOKEN))
+                    .orElseThrow(() -> new AuthorizationException(
+                            ErrorCode.UNAUTHORIZED, "Refresh Token does not exist in cookie."
+                    )).getValue();
+
+            JwtToken jwtToken = jwtService.reissueJwtToken(refreshToken);
+
+            response.setHeader("Set-Cookie", createCookie(ACCESS_TOKEN, jwtToken.getAccessToken()));
+            response.setHeader("Set-Cookie", createCookie(REFRESH_TOKEN, jwtToken.getRefreshToken()));
+
+            response.sendError(
+                    ErrorCode.RESET_CONTENT.getCode(),
+                    "The token was reissued because the access token expired."
+            );
+        } catch (SignatureVerificationException | InvalidClaimException e) {
+            System.out.println(LogUtils.makeErrorLog(
+                    ErrorCode.UNAUTHORIZED, "Invalid Access Token.", accessToken
+            ));
+
+            response.sendError(ErrorCode.UNAUTHORIZED.getCode(), "Invalid Access Token.");
+        }
+    }
+
+    private String createCookie(String tokenType, String token) {
+        int expireTime = tokenType.equals(ACCESS_TOKEN) ?
+                JwtProperty.ACCESS_TOKEN_EXPIRE_TIME : JwtProperty.REFRESH_TOKEN_EXPIRE_TIME;
+
+        return ResponseCookie.from(tokenType, token)
+                .maxAge(expireTime / 1000)
+                .httpOnly(true)
+                .path("/")
+                .sameSite("Lax")
+                .build()
+                .toString();
+    }
+}
