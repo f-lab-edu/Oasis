62c60a70d985b9f5a9c141dbd7e797febf1cf0fa
dev-msj <msj944@naver.com>
2023-03-12 01:11:56 +0900

    refactor(WebConfig) : allowedOrigins를 모두 허용하도록 변경
    추후 ncp에 배포하면 주소 반영할 예정

--- src/main/java/com/flab/oasis/config/WebConfig.java
+++ src/main/java/com/flab/oasis/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowCredentials(true)
                 .allowedMethods("*")
                 .allowedHeaders("*")
