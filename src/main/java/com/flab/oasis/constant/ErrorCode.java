9ed08bb9617f71b70edecca5a23de1fcacc49507
dev-msj <msj944@naver.com>
2023-03-17 04:34:58 +0900

    refactor(ErrorCode) : Service Unavailable 코드 추가

--- src/main/java/com/flab/oasis/constant/ErrorCode.java
+++ src/main/java/com/flab/oasis/constant/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
     RESET_CONTENT(205),
     UNAUTHORIZED(401),
-    NOT_FOUND(404);
+    NOT_FOUND(404),
+    SERVICE_UNAVAILABLE(503);
 
     final int code;
 }
