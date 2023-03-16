5f86b95cd18c08711cf4c54a9a8a6cf061ee7d17
dev-msj <msj944@naver.com>
2023-03-17 04:36:49 +0900

    refactor(UserAuthMapper) : parameter를 UserSession으로 받도록 변경

--- src/main/java/com/flab/oasis/mapper/UserAuthMapper.java
+++ src/main/java/com/flab/oasis/mapper/UserAuthMapper.java
@@ -1,10 +1,11 @@
 package com.flab.oasis.mapper;
 
 import com.flab.oasis.model.UserAuth;
+import com.flab.oasis.model.UserSession;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserAuthMapper {
     public UserAuth getUserAuthByUid(String uid);
-    public void updateRefreshToken(UserAuth userAuth);
+    public void updateRefreshToken(UserSession userSession);
 }
