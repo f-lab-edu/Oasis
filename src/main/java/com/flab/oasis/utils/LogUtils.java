b09e5fab2875c87f031117241c631b336ada012c
dev-msj <msj944@naver.com>
2023-03-17 06:57:19 +0900

    fix(LogUtils) : 줄 바꿈 적용되도록 수정

--- src/main/java/com/flab/oasis/utils/LogUtils.java
+++ src/main/java/com/flab/oasis/utils/LogUtils.java
@@ -12,14 +12,14 @@ public class LogUtils {
     }
 
     public static void info(String message, String value) {
-        LOGGER.info("Message: {}%nValue: {}", message, value);
+        LOGGER.info("Message: {}\nValue: {}", message, value);
     }
 
     public static void error(ErrorCode errorCode, String message) {
-        LOGGER.error("ErrorCode: {}({})%nMessage: {}", errorCode, errorCode.getCode(), message);
+        LOGGER.error("ErrorCode: {}({})\nMessage: {}", errorCode, errorCode.getCode(), message);
     }
 
     public static void error(ErrorCode errorCode, String message, String value) {
-        LOGGER.error("ErrorCode: {}({})%nMessage: {}%nValue: {}", errorCode, errorCode.getCode(), message, value);
+        LOGGER.error("ErrorCode: {}({})\nMessage: {}\nValue: {}", errorCode, errorCode.getCode(), message, value);
     }
 }
