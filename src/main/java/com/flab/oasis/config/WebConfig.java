bd812f234e0f3d2dd158923d81255f663646e5ac
dev-msj <msj944@naver.com>
2023-03-17 22:55:24 +0900

    refactor(WebConfig) : allow origin 모든 url 허용

--- src/main/java/com/flab/oasis/config/WebConfig.java
+++ src/main/java/com/flab/oasis/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .allowedMethods("*")
                 .allowedHeaders("*")
