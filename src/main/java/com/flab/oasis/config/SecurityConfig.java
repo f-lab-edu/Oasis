1a60880acf89df4045de5bb7800fae2432b1f2d0
dev-msj <msj944@naver.com>
2023-03-19 08:19:07 +0900

    refactor(SecurityConfig) : filter 설정 수정

--- src/main/java/com/flab/oasis/config/SecurityConfig.java
+++ src/main/java/com/flab/oasis/config/SecurityConfig.java
@@ -4,16 +4,18 @@ import com.flab.oasis.filter.JwtFilter;
 import com.flab.oasis.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@EnableWebSecurity // 스프링 시큐리티 필터를 스프링 필터체인에 등록
+@EnableWebSecurity(debug = true) // 스프링 시큐리티 필터를 스프링 필터체인에 등록
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtService jwtService;
@@ -22,33 +24,40 @@ public class SecurityConfig {
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf().disable() // csrf 인증 토큰 비활성
-                .headers().frameOptions().disable()
-                .and()
-                .cors().configurationSource(corsConfigurationSource()) // cross-origin resource sharing 허가
-                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 시큐리티 세션 비활성
                 .and()
                 .formLogin().disable() // 로그인 화면 비활성
                 .httpBasic().disable() // Authorization 헤더의 Basic 타입 사용 안함
-                .authorizeRequests()
-                .antMatchers("/api/auth").permitAll() // 인증 없이 접근 허용
-                .anyRequest().authenticated() // 허용된 uri 외에 모두 인증
+                .apply(new OasisAuthFilter())
                 .and()
-                .addFilterBefore(new JwtFilter(jwtService), UsernamePasswordAuthenticationFilter.class) // UsernamePasswordAuthenticationFilter 이전에 jwtFilter 실행
+                .authorizeRequests(auth -> auth
+                        .antMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 
-    public CorsConfigurationSource corsConfigurationSource() {
+    public class OasisAuthFilter extends AbstractHttpConfigurer<OasisAuthFilter, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+
+            http.addFilter(corsFilter()) // cross-origin resource sharing 허가
+                    .addFilter(new JwtFilter(authenticationManager, jwtService));
+        }
+    }
+
+    public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
 
-        return source;
+        return new CorsFilter(source);
     }
 }
