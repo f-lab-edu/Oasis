86700c3518d45ed26b891d8de47da0363b2ddbad
dev-msj <msj944@naver.com>
2023-04-05 03:05:00 +0900

    style(UserAuthControllerTest) : 사용하지 않는 import 제거

--- src/test/java/com/flab/oasis/login/UserAuthControllerTest.java
+++ src/test/java/com/flab/oasis/login/UserAuthControllerTest.java
@@ -18,7 +18,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
