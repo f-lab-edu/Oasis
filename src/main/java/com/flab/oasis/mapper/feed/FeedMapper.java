8d1a4697d796ed5caf926daada9dbcaef9e977dd
dev-msj <msj944@naver.com>
2023-04-14 17:03:37 +0900

    refactor : db를 book과 user로만 분리하도록 변경
    feed db 관련 정보 삭제

--- src/main/java/com/flab/oasis/mapper/feed/FeedMapper.java
+++
@@ -1,9 +0,0 @@
-package com.flab.oasis.mapper.feed;
-
-import com.flab.oasis.model.TestModel;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FeedMapper {
-    public TestModel dbConnectionTest();
-}
