71712fe973fbd9290ed1c446b8a3bd592dc28a31
dev-msj <msj944@naver.com>
2023-04-21 21:03:06 +0900

    fix(FeedUpdateRequest) : serialVersionUID 재생성

--- src/main/java/com/flab/oasis/model/FeedUpdateRequest.java
+++ src/main/java/com/flab/oasis/model/FeedUpdateRequest.java
@@ -8,8 +8,8 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class FeedUpdateRequest implements Serializable {
-    private static final long serialVersionUID = 5985546037987964046L;
-
+    private static final long serialVersionUID = 446411284628666675L;
+    
     private String uid;
     private String feedId;
     private String report;
