856cc9c21d9e91f4f90fd5ce48ed7dd7618d9737
dev-msj <msj944@naver.com>
2023-04-21 21:09:17 +0900

    feat : FeedDeleteRequest 모델 추가

---
+++ src/main/java/com/flab/oasis/model/FeedDeleteRequest.java
@@ -0,0 +1,15 @@
+package com.flab.oasis.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class FeedDeleteRequest implements Serializable {
+    private static final long serialVersionUID = 1600046029481345714L;
+
+    private String uid;
+    private String feedId;
+}
