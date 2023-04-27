f7c05324e81b9d559f7cfdd4540b7cd619cbde68
dev-msj <msj944@naver.com>
2023-04-27 16:06:39 +0900

    refactor(FeedService) : max feed id 생성 로직을 분리
    max feed id 생성 로직을 분리하여 디버깅이 가능하도록 수정

--- src/main/java/com/flab/oasis/service/FeedService.java
+++ src/main/java/com/flab/oasis/service/FeedService.java
@@ -1,11 +1,14 @@
 package com.flab.oasis.service;
 
 import com.flab.oasis.mapper.user.FeedMapper;
+import com.flab.oasis.model.Feed;
 import com.flab.oasis.model.FeedDeleteRequest;
 import com.flab.oasis.model.FeedUpdateRequest;
 import com.flab.oasis.model.FeedWriteRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,18 @@ public class FeedService {
     private final FeedMapper feedMapper;
 
     public void writeFeed(FeedWriteRequest feedWriteRequest) {
-        feedMapper.writeFeed(feedWriteRequest);
+        int maxFeedId = feedMapper.getMaxFeedIdByUid(feedWriteRequest.getUid());
+
+        feedMapper.writeFeed(
+                Feed.builder()
+                        .uid(feedWriteRequest.getUid())
+                        .feedId(maxFeedId + 1)
+                        .writeDate(new Date())
+                        .bookId(feedWriteRequest.getBookId())
+                        .report(feedWriteRequest.getReport())
+                        .feedLike(0)
+                        .build()
+        );
     }
 
     public void updateFeed(FeedUpdateRequest feedUpdateRequest) {
