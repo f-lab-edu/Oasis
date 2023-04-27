e87a2b5f7732b54725ca95ab3ffab5958de93c8b
dev-msj <msj944@naver.com>
2023-04-27 15:50:06 +0900

    refactor(FeedMapper) : max feed id를 구하는 쿼리를 분리

--- src/main/java/com/flab/oasis/mapper/user/FeedMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/FeedMapper.java
@@ -1,13 +1,14 @@
 package com.flab.oasis.mapper.user;
 
+import com.flab.oasis.model.Feed;
 import com.flab.oasis.model.FeedDeleteRequest;
 import com.flab.oasis.model.FeedUpdateRequest;
-import com.flab.oasis.model.FeedWriteRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FeedMapper {
-    public void writeFeed(FeedWriteRequest feedWriteRequest);
+    public int getMaxFeedIdByUid(String uid);
+    public void writeFeed(Feed feed);
     public void updateFeed(FeedUpdateRequest feedUpdateRequest);
     public void deleteFeed(FeedDeleteRequest feedDeleteRequest);
 }
