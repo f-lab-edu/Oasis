e371550cfb807ec3d275b3f9a82dc27f76438ae7
dev-msj <msj944@naver.com>
2023-04-27 20:25:16 +0900

    refactor(BookMapper) : 키워드 길이에 따라 검색 모드를 변경할 수 있도록 수정

--- src/main/java/com/flab/oasis/mapper/book/BookMapper.java
+++ src/main/java/com/flab/oasis/mapper/book/BookMapper.java
@@ -1,11 +1,12 @@
 package com.flab.oasis.mapper.book;
 
 import com.flab.oasis.model.Book;
+import com.flab.oasis.model.BookSearchRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface BookMapper {
-    public List<Book> findBookListByKeyword(String keyword);
+    public List<Book> findBookListByBookSearchRequest(BookSearchRequest bookSearchRequest);
 }
