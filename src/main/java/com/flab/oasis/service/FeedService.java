e485039b3fbe2725774f76f8b1aef32165dc1fae
dev-msj <msj944@naver.com>
2023-04-27 17:44:49 +0900

    refactor(FeedService) : transactional 어노테이션으로 writeFeed의 중복 무결성을 보장

--- src/main/java/com/flab/oasis/service/FeedService.java
+++ src/main/java/com/flab/oasis/service/FeedService.java
@@ -7,6 +7,7 @@ import com.flab.oasis.model.FeedUpdateRequest;
 import com.flab.oasis.model.FeedWriteRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -15,7 +16,10 @@ import java.util.Date;
 public class FeedService {
     private final FeedMapper feedMapper;
 
+    // 한 유저가 동시에 여러 개의 요청을 날리게 되면 중복된 feed id가 생성될 수 있기에 transaction으로 중복 무결성을 보장해줘야 한다.
+    @Transactional
     public void writeFeed(FeedWriteRequest feedWriteRequest) {
+        // 작성한 피드가 존재하지 않으면 0을 반환한다.
         int maxFeedId = feedMapper.getMaxFeedIdByUid(feedWriteRequest.getUid());
 
         feedMapper.writeFeed(
