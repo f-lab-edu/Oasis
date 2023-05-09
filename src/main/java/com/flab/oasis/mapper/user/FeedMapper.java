e7190c1c44db9dbb976bb3d20a46a4a7e5be980b
dev-msj <msj944@naver.com>
2023-05-10 04:05:45 +0900

    refactor(FeedMapper) : 파라메터를 Request 모델이 아닌 Feed로 변경

--- src/main/java/com/flab/oasis/mapper/user/FeedMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/FeedMapper.java
@@ -1,14 +1,12 @@
 package com.flab.oasis.mapper.user;
 
 import com.flab.oasis.model.Feed;
-import com.flab.oasis.model.FeedDeleteRequest;
-import com.flab.oasis.model.FeedUpdateRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FeedMapper {
     public Integer getMaxFeedIdByUid(String uid);
     public void writeFeed(Feed feed);
-    public void updateFeed(FeedUpdateRequest feedUpdateRequest);
-    public void deleteFeed(FeedDeleteRequest feedDeleteRequest);
+    public void updateFeed(Feed feed);
+    public void deleteFeed(Feed feed);
 }
