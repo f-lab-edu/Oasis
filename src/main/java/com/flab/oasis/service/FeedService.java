0ce05f258f1b1a45abeb8b9306630edc1b444557
dev-msj <msj944@naver.com>
2023-05-10 04:10:44 +0900

    refactor(FeedService) : threadlocal을 통해 uid를 가져오도록 수정
    Transactional 어노테이션 삭제
    update, delete 파라메터를 Feed로 넘기도록 수정

--- src/main/java/com/flab/oasis/service/FeedService.java
+++ src/main/java/com/flab/oasis/service/FeedService.java
@@ -1,5 +1,6 @@
 package com.flab.oasis.service;
 
+import com.flab.oasis.interceptor.UidInterceptor;
 import com.flab.oasis.mapper.user.FeedMapper;
 import com.flab.oasis.model.Feed;
 import com.flab.oasis.model.FeedDeleteRequest;
@@ -7,7 +8,6 @@ import com.flab.oasis.model.FeedUpdateRequest;
 import com.flab.oasis.model.FeedWriteRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.Optional;
@@ -17,13 +17,14 @@ import java.util.Optional;
 public class FeedService {
     private final FeedMapper feedMapper;
 
-    @Transactional
     public void writeFeed(FeedWriteRequest feedWriteRequest) {
-        int maxFeedId = Optional.ofNullable(feedMapper.getMaxFeedIdByUid(feedWriteRequest.getUid())).orElse(0);
+        String uid = UidInterceptor.STRING_THREAD_LOCAL.get();
+        int maxFeedId = Optional.ofNullable(feedMapper.getMaxFeedIdByUid(uid))
+                .orElse(0);
 
         feedMapper.writeFeed(
                 Feed.builder()
-                        .uid(feedWriteRequest.getUid())
+                        .uid(uid)
                         .feedId(maxFeedId + 1)
                         .writeDate(new Date())
                         .bookId(feedWriteRequest.getBookId())
@@ -34,10 +35,21 @@ public class FeedService {
     }
 
     public void updateFeed(FeedUpdateRequest feedUpdateRequest) {
-        feedMapper.updateFeed(feedUpdateRequest);
+        feedMapper.updateFeed(
+                Feed.builder()
+                        .uid(UidInterceptor.STRING_THREAD_LOCAL.get())
+                        .feedId(feedUpdateRequest.getFeedId())
+                        .report(feedUpdateRequest.getReport())
+                        .build()
+        );
     }
 
     public void deleteFeed(FeedDeleteRequest feedDeleteRequest) {
-        feedMapper.deleteFeed(feedDeleteRequest);
+        feedMapper.deleteFeed(
+                Feed.builder()
+                        .uid(UidInterceptor.STRING_THREAD_LOCAL.get())
+                        .feedId(feedDeleteRequest.getFeedId())
+                        .build()
+        );
     }
 }
