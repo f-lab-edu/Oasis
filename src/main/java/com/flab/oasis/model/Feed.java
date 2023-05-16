2ff18909528acc2a7e09167e2e81f218803429cb
dev-msj <msj944@naver.com>
2023-05-16 20:12:34 +0900

    refactor(Feed) : logging을 위해 ToString 추가

--- src/main/java/com/flab/oasis/model/Feed.java
+++ src/main/java/com/flab/oasis/model/Feed.java
@@ -10,6 +10,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Feed implements Serializable {
     private static final long serialVersionUID = 7061404739566319032L;
 
