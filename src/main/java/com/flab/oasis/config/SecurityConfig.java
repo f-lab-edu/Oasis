4ac96055dba89c774c84d0c571caf89d84a05a2b
dev-msj <msj944@naver.com>
2023-05-23 20:43:40 +0900

    style : Spring Security의 Oauth 제거

--- src/main/java/com/flab/oasis/config/SecurityConfig.java
+++ src/main/java/com/flab/oasis/config/SecurityConfig.java
@@ -2,9 +2,6 @@ package com.flab.oasis.config;
 
 import com.flab.oasis.filter.JwtFilter;
 import com.flab.oasis.service.JwtService;
-import com.flab.oasis.service.OAuth2Service;
-import com.flab.oasis.service.handler.OAuth2FailureHandler;
-import com.flab.oasis.service.handler.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,9 +18,6 @@ import org.springframework.web.filter.CorsFilter;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtService jwtService;
-    private final OAuth2Service oAuth2Service;
-    private final OAuth2SuccessHandler successHandler;
-    private final OAuth2FailureHandler failureHandler;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
@@ -38,13 +32,7 @@ public class SecurityConfig {
                 .authorizeRequests(auth -> auth
                         .antMatchers("/api/auth/**").permitAll() // '/api/auth' 경로는 인증 안함
                         .anyRequest().authenticated() // 그 외 경로는 모두 인증 필요
-                )
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(oAuth2Service)
-                .and()
-                .successHandler(successHandler)
-                .failureHandler(failureHandler);
+                );
 
         return httpSecurity.build();
     }
