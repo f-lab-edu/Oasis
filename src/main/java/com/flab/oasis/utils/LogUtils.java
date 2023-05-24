cbac5bbf5583f26a229ed86e8689c0886a9443ad
dev-msj <msj944@naver.com>
2023-05-24 14:05:56 +0900

    feat(LogUtils) : fatal 로그 추가
    fatal 로그를 통해 stacktrace를 출력하는 기능 추가

--- src/main/java/com/flab/oasis/utils/LogUtils.java
+++ src/main/java/com/flab/oasis/utils/LogUtils.java
@@ -4,6 +4,8 @@ import com.flab.oasis.constant.ErrorCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 public class LogUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogUtils.class);
 
@@ -27,5 +29,22 @@ public class LogUtils {
                 "Class: {}\nErrorCode: {}({})\nMessage: {}\nValue: {}",
                 exception.getName(), errorCode, errorCode.getCode(), message, value
         );
+    }
+
+    public static void fatal(Class<?> exception, ErrorCode errorCode, StackTraceElement[] stackTraceElements) {
+        String trace = parseStackTraceToString(stackTraceElements);
+        LOGGER.error(
+                "Class: {}\nErrorCode: {}({})\nTrace: \n{}",
+                exception.getName(), errorCode, errorCode.getCode(), trace
+        );
+    }
+
+    private static String parseStackTraceToString(StackTraceElement[] stackTraceElements) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Arrays.stream(stackTraceElements)
+                .forEach(e -> stringBuilder.append(e.toString()).append("\n"));
+
+        return stringBuilder.toString();
     }
 }
