17dcd64e7e0eef630f401647b19d81bef4577305
dev-msj <msj944@naver.com>
2023-05-30 17:45:23 +0900

    test : UserAuth의 Service와 Controller Test 병합

--- src/test/java/com/flab/oasis/login/UserAuthServiceTest.java
+++ src/test/java/com/flab/oasis/login/UserAuthServiceTest.java
@@ -1,12 +1,20 @@
 package com.flab.oasis.login;
 
 import com.flab.oasis.model.GoogleOAuthLoginRequest;
+import com.flab.oasis.model.JwtToken;
+import com.flab.oasis.model.UserAuth;
+import com.flab.oasis.model.UserLoginRequest;
 import com.flab.oasis.model.exception.AuthenticationException;
+import com.flab.oasis.repository.UserAuthRepository;
+import com.flab.oasis.service.JwtService;
 import com.flab.oasis.service.UserAuthService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -14,6 +22,48 @@ class UserAuthServiceTest {
     @InjectMocks
     UserAuthService userAuthService;
 
+    @Mock
+    UserAuthRepository userAuthRepository;
+
+    @Mock
+    JwtService jwtService;
+
+    @DisplayName("기본 로그인 테스트")
+    @Test
+    void testDefaultLogin() {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        JwtToken jwtToken = new JwtToken("", "");
+
+        // UserAuthService의 hashingPassword를 통해 해싱된 비밀번호
+        // parameter - password: null, salt: null
+        String hashingPassword = "5873df017b7dd58238f91328244dd841e497909e647418e9fa0a0b814baa26e7";
+
+        BDDMockito.given(userAuthRepository.getUserAuthByUid(userLoginRequest.getUid()))
+                .willReturn(UserAuth.builder().password(hashingPassword).build());
+        BDDMockito.given(jwtService.createJwtToken(userLoginRequest.getUid()))
+                .willReturn(jwtToken);
+
+        Assertions.assertEquals(
+                userAuthService.createJwtTokenByUserLoginRequest(userLoginRequest).getAccessToken(),
+                jwtToken.getAccessToken()
+        );
+    }
+
+    @DisplayName("기본 로그인 실패 테스트")
+    @Test
+    void testDefaultLoginFailure() {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+
+        BDDMockito.given(userAuthRepository.getUserAuthByUid(userLoginRequest.getUid()))
+                .willReturn(UserAuth.builder().password("").build());
+
+        Assertions.assertThrows(
+                AuthenticationException.class,
+                () -> userAuthService.createJwtTokenByUserLoginRequest(userLoginRequest)
+        );
+    }
+
+    @DisplayName("구글 ID 토큰 오류 테스트")
     @Test
     void testInvalidGoogleIdTokenError() {
         GoogleOAuthLoginRequest googleOAuthLoginRequest = new GoogleOAuthLoginRequest();
