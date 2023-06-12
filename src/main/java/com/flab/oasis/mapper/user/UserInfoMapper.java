2a127dd82d7e537129b72bbe8b788e38da690754
dev-msj <msj944@naver.com>
2023-06-13 03:58:17 +0900

    feat(UserInfoMapper) : USER_INFO 생성 및 조회 쿼리 추가

--- src/main/java/com/flab/oasis/mapper/user/UserInfoMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserInfoMapper.java
@@ -1,8 +1,11 @@
 package com.flab.oasis.mapper.user;
 
+import com.flab.oasis.model.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserInfoMapper {
     public boolean isExistsNickname(String nickname);
+    public void createUserInfo(UserInfo userInfo);
+    public UserInfo getUserInfoByUid(String uid);
 }
