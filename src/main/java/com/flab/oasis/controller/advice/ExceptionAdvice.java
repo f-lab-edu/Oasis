33165508b812460894ceb17acc0ed49f0eddf2ca
dev-msj <msj944@naver.com>
2023-06-16 16:53:10 +0900

    style(ExceptionAdvice) : 사용되지 않는 import 제거

--- src/main/java/com/flab/oasis/controller/advice/ExceptionAdvice.java
+++ src/main/java/com/flab/oasis/controller/advice/ExceptionAdvice.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.sql.SQLException;
-
 @RestControllerAdvice
 public class ExceptionAdvice {
     @ExceptionHandler(AuthenticationException.class)
