036d2511f7cc81fc14d8fc271212aea4b642ac87
dev-msj <msj944@naver.com>
2023-06-20 02:16:45 +0900

    feat : UserProfileResponse 추가

---
+++ src/main/java/com/flab/oasis/model/response/UserProfileResponse.java
@@ -0,0 +1,17 @@
+package com.flab.oasis.model.response;
+
+import com.flab.oasis.model.UserProfile;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@SuperBuilder
+public class UserProfileResponse extends BaseResponse implements Serializable {
+    private static final long serialVersionUID = -8608627323686074086L;
+
+    private UserProfile userProfile;
+}
