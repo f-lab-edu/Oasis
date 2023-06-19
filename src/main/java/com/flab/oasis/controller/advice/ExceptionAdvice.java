0791fc8460161cb04ebbc16f290590a026b6ef13
dev-msj <msj944@naver.com>
2023-06-20 04:48:09 +0900

    refactor(ExceptionAdvice) : 응답을 FailureResponse로 대체

--- src/main/java/com/flab/oasis/controller/advice/ExceptionAdvice.java
+++ src/main/java/com/flab/oasis/controller/advice/ExceptionAdvice.java
@@ -1,7 +1,7 @@
 package com.flab.oasis.controller.advice;
 
-import com.flab.oasis.model.GeneralResponse;
 import com.flab.oasis.model.exception.AuthenticationException;
+import com.flab.oasis.model.response.FailureResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<GeneralResponse<String>> handleAuthenticationException(AuthenticationException e) {
+    public ResponseEntity<FailureResponse> handleAuthenticationException(AuthenticationException e) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(
-                        GeneralResponse.<String>builder()
+                        FailureResponse.builder()
                                 .code(e.getErrorCode().getCode())
                                 .message(e.getMessage())
-                                .data(e.getValue())
+                                .value(e.getValue())
                                 .build()
                 );
     }
