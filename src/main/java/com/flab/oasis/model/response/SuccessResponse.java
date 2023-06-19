603c8680a28572351908af429d4f7bc6902c7630
dev-msj <msj944@naver.com>
2023-06-20 02:03:42 +0900

    feat : SuccessResponse 추가

---
+++ src/main/java/com/flab/oasis/model/response/SuccessResponse.java
@@ -0,0 +1,13 @@
+package com.flab.oasis.model.response;
+
+import com.flab.oasis.constant.ResponseCode;
+
+import java.io.Serializable;
+
+public class SuccessResponse extends BaseResponse implements Serializable {
+    private static final long serialVersionUID = -291461128620563480L;
+
+    public SuccessResponse() {
+        super(ResponseCode.OK.getCode(), "success");
+    }
+}
