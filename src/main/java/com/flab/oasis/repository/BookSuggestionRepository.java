138cb3873d39b588dd3a5ee31a67741b03596468
dev-msj <msj944@naver.com>
2023-06-22 05:54:19 +0900

    refactor : cacheable 어노테이션 적용

--- src/main/java/com/flab/oasis/repository/BookSuggestionRepository.java
+++ src/main/java/com/flab/oasis/repository/BookSuggestionRepository.java
@@ -1,49 +1,21 @@
 package com.flab.oasis.repository;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.flab.oasis.constant.ErrorCode;
 import com.flab.oasis.constant.SuggestionType;
 import com.flab.oasis.mapper.book.BookSuggestionMapper;
 import com.flab.oasis.model.BookSuggestion;
-import com.flab.oasis.utils.LogUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class BookSuggestionRepository {
-    private final RedisTemplate<SuggestionType, Object> redisTemplate;
-    private final ObjectMapper objectMapper;
     private final BookSuggestionMapper bookSuggestionMapper;
 
+    @Cacheable(cacheNames = "BookSuggestion", key = "#suggestionType", cacheManager = "redisCacheManager")
     public List<BookSuggestion> getBookSuggestionListBySuggestionType(SuggestionType suggestionType) {
-        try{
-            List<BookSuggestion> bookSuggestionList = objectMapper.convertValue(
-                    Optional.ofNullable(
-                            redisTemplate.opsForValue().get(suggestionType)
-                    ).orElse(
-                            getBookSuggestionListBySuggestionTypeFromDB(suggestionType)
-                    ),
-                    new TypeReference<List<BookSuggestion>>() {}
-
-            );
-
-            redisTemplate.opsForValue().set(suggestionType, bookSuggestionList);
-
-            return bookSuggestionList;
-        } catch (Exception e) {
-            LogUtils.error(e.getClass(), ErrorCode.SERVICE_UNAVAILABLE, e.getMessage());
-
-            return getBookSuggestionListBySuggestionTypeFromDB(suggestionType);
-        }
-    }
-
-    private List<BookSuggestion> getBookSuggestionListBySuggestionTypeFromDB(SuggestionType suggestionType) {
         return bookSuggestionMapper.getBookSuggestionBySuggestionType(suggestionType);
     }
 }
--- src/main/java/com/flab/oasis/repository/UserCategoryRepository.java
+++ src/main/java/com/flab/oasis/repository/UserCategoryRepository.java
@@ -1,43 +1,24 @@
 package com.flab.oasis.repository;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flab.oasis.mapper.user.UserCategoryMapper;
 import com.flab.oasis.model.UserCategory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class UserCategoryRepository {
-    private final RedisTemplate<String, Object> redisTemplate;
     private final UserCategoryMapper userCategoryMapper;
-    private final ObjectMapper objectMapper;
 
     public void createUserCategory(List<UserCategory> userCategoryList) {
         userCategoryMapper.createUserCategory(userCategoryList);
     }
 
+    @Cacheable(cacheNames = "UserCategory", key = "#uid", cacheManager = "redisCacheManager")
     public List<UserCategory> getUserCategoryListByUid(String uid) {
-        try {
-            List<UserCategory> userCategoryList = objectMapper.convertValue(
-                    Optional.ofNullable(
-                            redisTemplate.opsForHash().get("UserCategory", uid)
-                    ).orElse(
-                            userCategoryMapper.getUserCategoryListByUid(uid)
-                    ),
-                    new TypeReference<List<UserCategory>>() {}
-            );
-
-            redisTemplate.opsForHash().put("UserCategory", uid, userCategoryList);
-
-            return userCategoryList;
-        } catch (Exception e) {
-            return userCategoryMapper.getUserCategoryListByUid(uid);
-        }
+        return userCategoryMapper.getUserCategoryListByUid(uid);
     }
 }
