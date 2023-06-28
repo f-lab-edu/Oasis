9835eb2d79f25405b68801282cc336593211a85c
dev-msj <msj944@naver.com>
2023-06-29 07:57:37 +0900

    refactor(UserRelationService) : getRecommendUserList 로직 개선
    getRecommendUserList의 cache key를 위해 uid를 파라메터로 넘기도록 변경
    Cacheable을 통해 getRecommendUserList가 3분간 캐싱되도록 수정
    getRecommendUserList 로직 정리

--- src/main/java/com/flab/oasis/service/UserRelationService.java
+++ src/main/java/com/flab/oasis/service/UserRelationService.java
@@ -1,16 +1,15 @@
 package com.flab.oasis.service;
 
-import com.flab.oasis.constant.ErrorCode;
 import com.flab.oasis.mapper.user.FeedMapper;
 import com.flab.oasis.model.*;
-import com.flab.oasis.model.exception.NotFoundException;
 import com.flab.oasis.repository.UserCategoryRepository;
 import com.flab.oasis.repository.UserRelationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,11 +20,18 @@ public class UserRelationService {
     private final UserCategoryRepository userCategoryRepository;
     private final UserRelationRepository userRelationRepository;
 
-    public List<String> getRecommendUserList() {
-        String uid = userAuthService.getAuthenticatedUid();
-
+    @Cacheable(cacheNames = "recommendUserCache", key = "#uid", cacheManager = "ehCacheCacheManager")
+    public List<String> getRecommendUserList(String uid) {
         List<UserCategory> userCategoryList = userCategoryRepository.getUserCategoryListByUid(uid);
         List<UserRelation> userRelationList = userRelationRepository.getUserRelationListByUid(uid);
+
+        if (userCategoryList.isEmpty()) {
+            return getUidListFromRecommendUserList(
+                    getRecommendUserAsManyAsNeed(
+                            getUidListFromUserRelationList(userRelationList), 30
+                    )
+            );
+        }
 
         List<String> overlappingCategoryUserList = userCategoryRepository.getUidListWithOverlappingBookCategory(
                 OverlappingCategoryUserSelect.builder()
@@ -35,55 +41,29 @@ public class UserRelationService {
                         .build()
         );
 
-        List<RecommendUser> recommendUserList = new ArrayList<>();
-
         if (overlappingCategoryUserList.isEmpty()) {
-            recommendUserList.addAll(getRecommendUserAsManyAsNeed(uid, 30));
-        } else {
-            List<UserCategoryCount> userCategoryCountList = userCategoryRepository.getUserCategoryCountList(
-                    UserCategoryCountSelect.builder()
-                            .uidList(overlappingCategoryUserList)
-                            .userCategoryList(userCategoryList)
-                            .build()
-            );
-
-            List<FeedCount> feedCountList = feedMapper.getFeedCountList(
-                    FeedCountSelect.builder()
-                            .uidList(overlappingCategoryUserList)
-                            .needSize(-1)
-                            .build()
-            );
-
-            recommendUserList.addAll(
-                    userCategoryCountList.stream()
-                            .map(
-                                    userCategoryCount -> RecommendUser.builder()
-                                            .uid(userCategoryCount.getUid())
-                                            .categoryCount(userCategoryCount.getBookCategoryCount())
-                                            .build()
-                            ).sorted().collect(Collectors.toList())
-            );
-
-            feedCountList.forEach(
-                    feedCount -> recommendUserList.stream()
-                                .filter(user -> user.getUid().equals(feedCount.getUid()))
-                                .findFirst()
-                                .orElseThrow(() -> new NotFoundException(
-                                        ErrorCode.NOT_FOUND, "Can't find recommend user's uid", feedCount.getUid()
-                                ))
-                                .setFeedCount(feedCount.getFeedCount())
+            return getUidListFromRecommendUserList(
+                    getRecommendUserAsManyAsNeed(
+                            getUidListFromUserRelationList(userRelationList), 30
+                    )
             );
         }
 
+        List<RecommendUser> recommendUserList = makeRecommendUserList(userCategoryList, overlappingCategoryUserList);
+
+        // 추천 유저가 30명이 안될 경우, 부족한 추천 user를 채운다.
         if (recommendUserList.size() < 30) {
+            List<String> uidList = getUidListFromUserRelationList(userRelationList);
+            uidList.addAll(
+                    getUidListFromRecommendUserList(recommendUserList)
+            );
+
             recommendUserList.addAll(
-                    getRecommendUserAsManyAsNeed(uid, 30 - recommendUserList.size())
+                    getRecommendUserAsManyAsNeed(uidList, 30 - recommendUserList.size())
             );
         }
 
-        return recommendUserList.stream()
-                .map(RecommendUser::getUid)
-                .collect(Collectors.toList());
+        return getUidListFromRecommendUserList(recommendUserList);
     }
 
     public void createUserRelation(UserRelation userRelation) {
@@ -92,21 +72,64 @@ public class UserRelationService {
         userRelationRepository.createUserRelation(userRelation);
     }
 
-    private List<RecommendUser> getRecommendUserAsManyAsNeed(String uid, int needSize) {
+    // '본인 | relation이 있는 user | 생성된 recommend user'를 제외하고 needSize만큼 recommend user를 가져온다.
+    private List<RecommendUser> getRecommendUserAsManyAsNeed(List<String> uidList, int needSize) {
+        uidList.add(userAuthService.getAuthenticatedUid());
+
         List<FeedCount> feedCountList = feedMapper.getFeedCountList(
                 FeedCountSelect.builder()
-                        .uid(uid)
+                        .uidList(uidList)
                         .needSize(needSize)
                         .build()
         );
 
         return feedCountList.stream()
-                .map(
-                        feedCount -> RecommendUser.builder()
+                .map(feedCount -> RecommendUser.builder()
                                 .uid(feedCount.getUid())
                                 .feedCount(feedCount.getFeedCount())
                                 .build()
-                ).sorted()
+                )
+                .sorted()
                 .collect(Collectors.toList());
+    }
+
+    private List<String> getUidListFromUserRelationList(List<UserRelation> userRelationList) {
+        return userRelationList.stream()
+                .map(UserRelation::getRelationUser)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getUidListFromRecommendUserList(List<RecommendUser> recommendUserList) {
+        return recommendUserList.stream()
+                .map(RecommendUser::getUid)
+                .collect(Collectors.toList());
+    }
+
+    private List<RecommendUser> makeRecommendUserList(
+            List<UserCategory> userCategoryList, List<String> overlappingCategoryUserList) {
+        Map<String, Integer> userCategoryCountMap = userCategoryRepository.getUserCategoryCountList(
+                UserCategoryCountSelect.builder()
+                        .uidList(overlappingCategoryUserList)
+                        .userCategoryList(userCategoryList)
+                        .build()
+        )
+                .stream()
+                .collect(Collectors.toMap(UserCategoryCount::getUid, UserCategoryCount::getBookCategoryCount));
+
+        List<FeedCount> feedCountList = feedMapper.getFeedCountList(
+                FeedCountSelect.builder()
+                        .uidList(overlappingCategoryUserList)
+                        .needSize(-1)
+                        .build()
+        );
+
+        return feedCountList.stream()
+                .map(feedCount -> RecommendUser.builder()
+                        .uid(feedCount.getUid())
+                        .feedCount(feedCount.getFeedCount())
+                        .categoryCount(userCategoryCountMap.get(feedCount.getUid()))
+                        .build()
+                ).
+                collect(Collectors.toList());
     }
 }
