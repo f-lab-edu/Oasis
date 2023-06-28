1a453ecf295a6e26a808142f9c1fcdac48c66d3d
dev-msj <msj944@naver.com>
2023-06-29 01:07:02 +0900

    feat : UserRelation 추가

---
+++ src/main/java/com/flab/oasis/model/UserRelation.java
@@ -0,0 +1,16 @@
+package com.flab.oasis.model;
+
+import com.flab.oasis.constant.RelationType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class UserRelation implements Serializable {
+    private static final long serialVersionUID = 9092423039001303261L;
+
+    private String uid;
+    private RelationType relationType;
+}
