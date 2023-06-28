e4c90eeae57ccc525e86dd5f792ba6ca8c363e60
dev-msj <msj944@naver.com>
2023-06-29 05:28:53 +0900

    feat(UserCategoryMapper) : getUserCategoryCountList 쿼리 추가

--- src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
@@ -1,7 +1,9 @@
 package com.flab.oasis.mapper.user;
 
 import com.flab.oasis.model.OverlappingCategoryUserSelect;
+import com.flab.oasis.model.UserCategoryCountSelect;
 import com.flab.oasis.model.UserCategory;
+import com.flab.oasis.model.UserCategoryCount;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -11,4 +13,5 @@ public interface UserCategoryMapper {
     public void createUserCategory(List<UserCategory> userCategoryList);
     public List<UserCategory> getUserCategoryListByUid(String uid);
     public List<String> getUidListWithOverlappingBookCategory(OverlappingCategoryUserSelect overlappingCategoryUserSelect);
+    public List<UserCategoryCount> getUserCategoryCountList(UserCategoryCountSelect userCategoryCountSelect);
 }
