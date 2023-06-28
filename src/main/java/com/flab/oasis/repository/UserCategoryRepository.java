56e26d0f3af2a2ca45343a8cab68419fffe04c98
dev-msj <msj944@naver.com>
2023-06-29 05:29:11 +0900

    feat(UserCategoryRepository) : getUserCategoryCountList 메서드 추가

--- src/main/java/com/flab/oasis/repository/UserCategoryRepository.java
+++ src/main/java/com/flab/oasis/repository/UserCategoryRepository.java
@@ -2,7 +2,9 @@ package com.flab.oasis.repository;
 
 import com.flab.oasis.mapper.user.UserCategoryMapper;
 import com.flab.oasis.model.OverlappingCategoryUserSelect;
+import com.flab.oasis.model.UserCategoryCountSelect;
 import com.flab.oasis.model.UserCategory;
+import com.flab.oasis.model.UserCategoryCount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
@@ -26,5 +28,9 @@ public class UserCategoryRepository {
     public List<String> getUidListWithOverlappingBookCategory(
             OverlappingCategoryUserSelect overlappingCategoryUserSelect) {
         return userCategoryMapper.getUidListWithOverlappingBookCategory(overlappingCategoryUserSelect);
+    }
+
+    public List<UserCategoryCount> getUserCategoryCountList(UserCategoryCountSelect userCategoryCountSelect) {
+        return userCategoryMapper.getUserCategoryCountList(userCategoryCountSelect);
     }
 }
