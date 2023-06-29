5af9b2d467c1e8d844bf6f6555130d0d09bb69a6
dev-msj <msj944@naver.com>
2023-06-29 18:58:43 +0900

    feat : UsersFeedCountSelect 모델 추가

---
+++ src/main/java/com/flab/oasis/model/UsersFeedCountSelect.java
@@ -0,0 +1,18 @@
+package com.flab.oasis.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsersFeedCountSelect implements Serializable {
+    private static final long serialVersionUID = 7061404739566319032L;
+
+    private List<String> uidList;
+    private int needSize;
+}
