ba763e2d28a9ad4055e7f6eef14ae554641b2e63
dev-msj <msj944@naver.com>
2023-06-29 16:08:56 +0900

    feat : getRecommendUserList 쿼리 추가
    추천 유저를 Feed Count 순으로 needSize만큼 가져온다.
    Feed는 nullable하므로 join하지 않고 subquery로 사용한다.

--- src/main/java/com/flab/oasis/mapper/user/UserInfoMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserInfoMapper.java
@@ -1,11 +1,15 @@
 package com.flab.oasis.mapper.user;
 
+import com.flab.oasis.model.RecommendUserSelect;
 import com.flab.oasis.model.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UserInfoMapper {
     public boolean isExistsNickname(String nickname);
     public void createUserInfo(UserInfo userInfo);
     public UserInfo getUserInfoByUid(String uid);
+    public List<String> getRecommendUserList(RecommendUserSelect recommendUserSelect);
 }
