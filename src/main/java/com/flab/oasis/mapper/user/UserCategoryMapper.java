dab52116e5be2e89ff4b304e018675fcb6d58886
dev-msj <msj944@naver.com>
2023-07-04 18:28:48 +0900

    refactor(UserCategoryMapper) : getUserCategoryCountList 쿼리 수정
    메서드명 변경
    - getUserCategoryCountList -> getOverlappingUserCategoryList
    
    쿼리 변경
    - parameter로 전달받은 overlappingCategoryUserList와 user_category를 join하여
      overlappingCategoryUserList에 해당하는 UserCategory List를 반환하도록 수정

--- src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
@@ -1,8 +1,6 @@
 package com.flab.oasis.mapper.user;
 
 import com.flab.oasis.model.UserCategory;
-import com.flab.oasis.model.UserCategoryCount;
-import com.flab.oasis.model.UserCategoryCountSelect;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,5 +10,5 @@ public interface UserCategoryMapper {
     public void createUserCategory(List<UserCategory> userCategoryList);
     public List<UserCategory> getUserCategoryListByUid(String uid);
     public List<String> getUidListWithOverlappingBookCategory(String uid);
-    public List<UserCategoryCount> getUserCategoryCountList(UserCategoryCountSelect userCategoryCountSelect);
+    public List<UserCategory> getOverlappingUserCategoryList(List<String> overlappingCategoryUserList);
 }
