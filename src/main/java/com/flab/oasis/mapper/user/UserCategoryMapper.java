7860ab1a54ab10f1a30a2d48c043e7ff9b007ced
dev-msj <msj944@naver.com>
2023-07-04 14:12:54 +0900

    refactor(UserCategoryMapper) : getUidListWithOverlappingBookCategory 쿼리 수정
    join을 활용해 겹치는 카테고리가 있는 유저 목록을 가져오도록 수정
    관계가 존재하는 유저는 java 로직을 활용하여 필터링 하도록 수정

--- src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
@@ -1,9 +1,8 @@
 package com.flab.oasis.mapper.user;
 
-import com.flab.oasis.model.OverlappingCategoryUserSelect;
-import com.flab.oasis.model.UserCategoryCountSelect;
 import com.flab.oasis.model.UserCategory;
 import com.flab.oasis.model.UserCategoryCount;
+import com.flab.oasis.model.UserCategoryCountSelect;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,6 +11,6 @@ import java.util.List;
 public interface UserCategoryMapper {
     public void createUserCategory(List<UserCategory> userCategoryList);
     public List<UserCategory> getUserCategoryListByUid(String uid);
-    public List<String> getUidListWithOverlappingBookCategory(OverlappingCategoryUserSelect overlappingCategoryUserSelect);
+    public List<String> getUidListWithOverlappingBookCategory(String uid);
     public List<UserCategoryCount> getUserCategoryCountList(UserCategoryCountSelect userCategoryCountSelect);
 }
