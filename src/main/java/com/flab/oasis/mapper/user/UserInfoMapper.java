83caf41d732b007a3904948dc7aaaf20c6f13c82
dev-msj <msj944@naver.com>
2023-07-05 05:37:33 +0900

    refactor(UserInfoMapper) : getUserFeedCountList 쿼리 변경
    메서드명 변경
    - getUserFeedCountList -> getDefaultRecommendUserExcludeUidList
    
    쿼리 변경
    - exclude uid list를 제외한 유저를 피드 작성량 순으로 정렬하여 30명을 가져오도록 수정

--- src/main/java/com/flab/oasis/mapper/user/UserInfoMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserInfoMapper.java
@@ -1,7 +1,5 @@
 package com.flab.oasis.mapper.user;
 
-import com.flab.oasis.model.UserFeedCount;
-import com.flab.oasis.model.UserFeedCountSelect;
 import com.flab.oasis.model.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -12,5 +10,5 @@ public interface UserInfoMapper {
     public boolean isExistsNickname(String nickname);
     public void createUserInfo(UserInfo userInfo);
     public UserInfo getUserInfoByUid(String uid);
-    public List<UserFeedCount> getUserFeedCountList(UserFeedCountSelect userFeedCountSelect);
+    public List<String> getDefaultRecommendUserExcludeUidList(List<String> excludeUidList);
 }
