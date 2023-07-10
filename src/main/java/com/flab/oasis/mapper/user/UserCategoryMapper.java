6435e5f2961d5fe1e655b4945cd149e03d27c9a7
dev-msj <msj944@naver.com>
2023-07-10 19:06:42 +0900

    feat(UserCategoryMapper) : getUidListByBookCategory 쿼리 추가
    카테고리를 생성 또는 수정한 일자가 6개월 이내인 유저를 카테고리별로 가져오는 쿼리 작성

--- src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
+++ src/main/java/com/flab/oasis/mapper/user/UserCategoryMapper.java
@@ -1,5 +1,6 @@
 package com.flab.oasis.mapper.user;
 
+import com.flab.oasis.constant.BookCategory;
 import com.flab.oasis.model.UserCategory;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,6 +10,7 @@ import java.util.List;
 public interface UserCategoryMapper {
     public void createUserCategory(List<UserCategory> userCategoryList);
     public List<UserCategory> getUserCategoryListByUid(String uid);
+    public List<String> getUidListByBookCategory(BookCategory bookCategory);
     public List<String> getUidListIfOverlappingBookCategory(String uid);
     public List<UserCategory> getUserCategoryListByUidList(List<String> uidList);
 }
