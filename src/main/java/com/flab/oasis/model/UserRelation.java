39b49aad3b95f3a8b7f0f616d76daed48c80d3e3
dev-msj <msj944@naver.com>
2023-07-10 21:26:43 +0900

    refactor(UserRelation) : Builder 어노테이션 추가

--- src/main/java/com/flab/oasis/model/UserRelation.java
+++ src/main/java/com/flab/oasis/model/UserRelation.java
@@ -1,6 +1,7 @@
 package com.flab.oasis.model;
 
 import com.flab.oasis.constant.RelationType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@Builder
 public class UserRelation implements Serializable {
     private static final long serialVersionUID = 9092423039001303261L;
 
