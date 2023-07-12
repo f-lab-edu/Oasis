f21100e96889b5466c91a57755413bb61a3ab32e
dev-msj <msj944@naver.com>
2023-07-12 16:59:29 +0900

    fix : Serializable 문제 해결
    RecommendCandidateUser
    - NoArgsConstructor, AllArgsConstructor 어노테이션 추가
    - LocalDateTime Serializer 관련 패키지 추가
    - LocalDateTime Serializer 어노테이션 추가
    
    UserRelation
    - NoArgsConstructor, AllArgsConstructor 어노테이션 추가

--- src/main/java/com/flab/oasis/model/UserRelation.java
+++ src/main/java/com/flab/oasis/model/UserRelation.java
@@ -1,14 +1,14 @@
 package com.flab.oasis.model;
 
 import com.flab.oasis.constant.RelationType;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserRelation implements Serializable {
     private static final long serialVersionUID = 9092423039001303261L;
